22023fdc8d3e5ade6306fdab4dba5503dd40dc27
sterni <sternenseemann@systemli.org>
2024-12-26 13:23:24 +0100

    docs(sterni/mblog/maildir): add more references
    Change-Id: Ie3367d05c1cf76ee98501f0527c1da6f3282b4fe
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/12920
    Reviewed-by: sterni <sternenseemann@systemli.org>
    Autosubmit: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI

--- users/sterni/mblog/maildir.lisp
+++ users/sterni/mblog/maildir.lisp
@@ -4,12 +4,14 @@
 (in-package :maildir)
 (declaim (optimize (safety 3)))
 
+;; https://cr.yp.to/proto/maildir.html
+;; http://qmail.org/qmail-manual-html/man5/maildir.html
 (defun list (dir)
   "Returns a list of pathnames to messages in a maildir. The messages are
-  returned in no guaranteed order. Note that this function doesn't fully
-  implement the behavior prescribed by maildir(5): It only looks at `cur`
-  and `new` and won't clean up `tmp` nor move files from `new` to `cur`,
-  since it is strictly read-only."
+returned in no guaranteed order. Note that this function doesn't fully
+implement the behavior prescribed by maildir(5): It only looks at `cur`
+and `new` and won't clean up `tmp` nor move files from `new` to `cur`,
+since it is strictly read-only. In this, it follows mlist(1)."
   (flet ((subdir-contents (subdir)
            (directory
             (merge-pathnames
