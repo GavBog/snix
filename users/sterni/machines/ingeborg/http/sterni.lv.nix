984c930a7c9a9923e70400c30ff5cad1ca5c6325
sterni <sternenseemann@systemli.org>
2025-02-27 02:18:34 +0100

    feat(sterni/nix/build): add my simplistic nix website authoring tool
    I am already using this outside of depot where it isn't under VCS. It
    makes sense to canonicalize it, also to ensure that it stays somewhat
    generic.
    
    Change-Id: I5595d98ab4198794c395feb4d3c08df1e2d01a36
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13184
    Autosubmit: sterni <sternenseemann@systemli.org>
    Reviewed-by: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI

--- users/sterni/machines/ingeborg/http/sterni.lv.nix
+++ users/sterni/machines/ingeborg/http/sterni.lv.nix
@@ -15,16 +15,16 @@ in
     services.nginx.virtualHosts."sterni.lv" = {
       enableACME = true;
       forceSSL = true;
-      root = pkgs.writeTextFile {
-        name = "sterni.lv-http-root";
-        destination = "/index.html";
-        text = <html> { } [
-          (<head> { } [
-            (<meta> { charset = "utf-8"; } null)
-            (<title> { } "no thoughts")
-          ])
-          (<body> { } "🦩")
-        ];
+      root = depot.users.sterni.nix.build.website "sterni.lv" { } {
+        "index.html" = { ... }: pkgs.writeText "index.html" (
+          <html> { } [
+            (<head> { } [
+              (<meta> { charset = "utf-8"; } null)
+              (<title> { } "no thoughts")
+            ])
+            (<body> { } "🦩")
+          ]
+        );
       };
       # TODO(sterni): tmp.sterni.lv
       locations."/tmp/".root = toString /srv/http;
