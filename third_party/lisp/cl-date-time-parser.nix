e812e0514dcd04cda26c0313b626e1a0fe3a1d6e
sterni <sternenseemann@systemli.org>
2025-03-09 16:39:53 +0100

    chore(3p/lisp/cl-date-time-parser): use nixpkgs/quicklisp src
    Change-Id: Ic242b5e9d0dfac12f5f68243380d1dbc4c9f18df
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13231
    Autosubmit: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

--- third_party/lisp/cl-date-time-parser.nix
+++ third_party/lisp/cl-date-time-parser.nix
@@ -4,10 +4,7 @@ depot.nix.buildLisp.library {
   name = "cl-date-time-parser";
 
   srcs = [
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/tkych/cl-date-time-parser/00d6fc70b599f460fdf13cf0cf7e6bf843312410/date-time-parser.lisp";
-      sha256 = "0zrkv1q3sx5ksijxhw45ixf1hy5b9biii6i6v41h12q6pbkfqz69";
-    })
+    "${pkgs.srcOnly pkgs.sbcl.pkgs.cl-date-time-parser}/date-time-parser.lisp"
   ];
 
   deps = [
