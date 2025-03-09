93282ac6f01792c2cd2f5c98bf15e5a0f7882d99
sterni <sternenseemann@systemli.org>
2025-03-09 16:43:44 +0100

    chore(3p/lisp/lass): get src from nixpkgs tracked quicklisp
    Upstream has removed its fancy support for units and calculations. It
    appears panettone does not rely on this at all.
    
    Change-Id: I9ee3637ba44d1d3c225e6bbfc02b820f3a7d028c
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13230
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI
    Autosubmit: sterni <sternenseemann@systemli.org>

--- third_party/lisp/lass.nix
+++ third_party/lisp/lass.nix
@@ -1,13 +1,17 @@
 { depot, pkgs, ... }:
 
 let
-  src = pkgs.fetchFromGitHub {
-    owner = "Shinmera";
-    repo = "LASS";
-    rev = "f51b9e941ee0a2a1f76ba814dcef22f9fb5f69bf";
-    sha256 = "11mxzyx34ynsfsrs8pgrarqi9s442vkpmh7kdpzvarhj7i97g8yx";
+  src = pkgs.applyPatches {
+    src = pkgs.srcOnly pkgs.sbcl.pkgs.lass;
+    patches = [
+      # https://github.com/Shinmera/LASS/pull/22
+      (pkgs.fetchpatch {
+        name = "lass-fix-ccl-build.patch";
+        url = "https://github.com/Shinmera/LASS/commit/957afc830f0517f1053cdd8605af1dc5e457527f.patch";
+        sha256 = "06fp0rnqqvai08lr6aldzga2xc9dxdfffrpgs3rha9gp0xmvlz43";
+      })
+    ];
   };
-
 in
 depot.nix.buildLisp.library {
   name = "lass";
@@ -15,8 +19,6 @@ depot.nix.buildLisp.library {
   deps = with depot.third_party.lisp; [
     trivial-indent
     trivial-mimes
-    physical-quantities
-    parse-float
     cl-base64
     (depot.nix.buildLisp.bundled "asdf")
   ];
@@ -29,7 +31,6 @@ depot.nix.buildLisp.library {
     "writer.lisp"
     "lass.lisp"
     "special.lisp"
-    "units.lisp"
     "asdf.lisp"
   ];
 }
