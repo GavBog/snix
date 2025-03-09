8e93f2ac6de05d9f0b37cc001fa82402319907b1
sterni <sternenseemann@systemli.org>
2025-03-09 16:44:54 +0100

    chore(3p/lisp/routes): get src from nixpkgs tracked quicklisp
    Change-Id: I1d8030fb0446e7aee952aa31c8ca9ed2b962b439
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13228
    Tested-by: BuildkiteCI
    Autosubmit: sterni <sternenseemann@systemli.org>
    Reviewed-by: tazjin <tazjin@tvl.su>

--- third_party/lisp/routes.nix
+++ third_party/lisp/routes.nix
@@ -4,12 +4,7 @@ let
 
   src = pkgs.applyPatches {
     name = "routes-source";
-    src = pkgs.fetchFromGitHub {
-      owner = "archimag";
-      repo = "cl-routes";
-      rev = "1b79e85aa653e1ec87e21ca745abe51547866fa9";
-      sha256 = "1zpk3cp2v8hm50ppjl10yxr437vv4552r8hylvizglzrq2ibsbr1";
-    };
+    src = pkgs.srcOnly pkgs.sbcl.pkgs.routes;
 
     patches = [
       (pkgs.fetchpatch {
