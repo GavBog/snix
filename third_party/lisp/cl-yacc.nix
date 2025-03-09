0bcd5a82d409042bf0b8793c4d87e903c1cc81f3
sterni <sternenseemann@systemli.org>
2025-03-09 16:25:11 +0100

    chore(3p/sources/cl-yacc): bump source
    No substantial changes, just some documentation updates.
    
    Change-Id: I96e27d8c59e4cfb9a34750dbd10f4d3e51e36619
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13233
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI
    Autosubmit: sterni <sternenseemann@systemli.org>

--- third_party/lisp/cl-yacc.nix
+++ third_party/lisp/cl-yacc.nix
@@ -4,8 +4,8 @@ let
   src = pkgs.fetchFromGitHub {
     owner = "jech";
     repo = "cl-yacc";
-    rev = "1334f5469251ffb3f8738a682dc8ee646cb26635";
-    sha256 = "16946pzf8vvadnyfayvj8rbh4zjzw90h0azz2qk1mxrvhh5wklib";
+    rev = "1812e05317dcab1e97905625c018c043d71f9187"; # 2023-01-08
+    sha256 = "1f974ysi7mlrksnqg63iwwxgbypkng4n240q29imkrz6m5pwdig7";
   };
 in
 depot.nix.buildLisp.library {
