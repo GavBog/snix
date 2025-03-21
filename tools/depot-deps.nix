1f380dcad3030832afb1d9b09a5b5d7390a8fae3
Florian Klink <flokli@flokli.de>
2025-03-21 18:48:21 +0000

    chore(tools/depot-deps): drop some things
    Some of these don't even exist anymore (like nint or josh-filter).
    People who want to use these can have them in their global user
    environment.
    
    Change-Id: I02119ad13beeac352a2184fda9c7a7146d89a8d5
    Reviewed-on: https://cl.snix.dev/c/snix/+/30243
    Reviewed-by: Ilan Joselevich <personal@ilanjoselevich.com>
    Autosubmit: Florian Klink <flokli@flokli.de>
    Tested-by: besadii

--- tools/depot-deps.nix
+++ tools/depot-deps.nix
@@ -6,15 +6,10 @@ depot.nix.lazy-deps {
   age-keygen.attr = "third_party.nixpkgs.age";
   age.attr = "third_party.nixpkgs.age";
   depotfmt.attr = "tools.depotfmt";
-  git-review.attr = "third_party.nixpkgs.git-review";
-  gerrit-update.attr = "tools.gerrit-update";
   gerrit.attr = "tools.gerrit-cli";
-  josh-filter.attr = "third_party.nixpkgs.josh";
   mg.attr = "tools.magrathea";
-  nint.attr = "nix.nint";
   niv.attr = "third_party.nixpkgs.niv";
   nixpkgs-fmt.attr = "third_party.nixpkgs.nixpkgs-fmt";
-  rink.attr = "third_party.nixpkgs.rink";
 
   tf-buildkite = {
     attr = "ops.buildkite.terraform";
