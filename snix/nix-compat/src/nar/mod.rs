759f15390c45139740b77141431d36c6d5cd3d1b
Florian Klink <flokli@flokli.de>
2025-05-04 02:37:37 +0300

    feat(nix-compat/nar): add copy functions
    This allows piping NAR data through a reader, and writing it back out to
    a writer.
    
    It can be used to validate a NAR to be syntactically correct, or to read
    exactly to the end of a NAR file if the size is not given externally.
    
    Change-Id: I0fc8d58e68783400d1cfee75c860138915974f3d
    Reviewed-on: https://cl.snix.dev/c/snix/+/30423
    Tested-by: besadii
    Reviewed-by: edef <edef@edef.eu>
    Autosubmit: Florian Klink <flokli@flokli.de>

--- snix/nix-compat/src/nar/mod.rs
+++ snix/nix-compat/src/nar/mod.rs
@@ -1,5 +1,13 @@
 pub(crate) mod wire;
 
+mod copy;
 pub mod listing;
 pub mod reader;
 pub mod writer;
+
+#[cfg(all(feature = "async", feature = "wire"))]
+mod copy_async;
+
+pub use copy::copy;
+#[cfg(all(feature = "async", feature = "wire"))]
+pub use copy_async::copy as copy_async;
