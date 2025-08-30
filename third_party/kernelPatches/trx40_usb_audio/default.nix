5891741f8298da3b2d9f3d33aa7c52a671912ee5
Florian Klink <flokli@flokli.de>
2025-08-30 18:22:17 +0300

    chore(3p/kernelPatches): drop
    Change-Id: Ic72c15322f07ac12587ace7276fba843ded87a0c
    Reviewed-on: https://cl.snix.dev/c/snix/+/30716
    Reviewed-by: Ilan Joselevich <personal@ilanjoselevich.com>
    Autosubmit: Florian Klink <flokli@flokli.de>
    Tested-by: besadii

--- third_party/kernelPatches/trx40_usb_audio/default.nix
+++
@@ -1,9 +0,0 @@
-# This patch adds the ASUS TRX40 Prime Pro Whatever Edition
-# motherboard to the list of boards for which the USB Audio connector
-# map has been fixed.
-{ ... }:
-
-{
-  name = "trx40_usb_audio";
-  patch = ./trx40_usb_audio.patch;
-}
