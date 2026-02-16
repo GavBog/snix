e9c95ada99e17ca88d22e2e56e149279550dfd99
Florian Klink <flokli@flokli.de>
2026-02-16 23:18:07 +0200

    feat(ops/pipelines/depot): anchor on build03
    Change-Id: Icad61374c064770c49ff34ef1ca491d6c7a78345
    Reviewed-on: https://cl.snix.dev/c/snix/+/30981
    Autosubmit: Florian Klink <flokli@flokli.de>
    Tested-by: besadii
    Reviewed-by: Ilan Joselevich <personal@ilanjoselevich.com>

--- ops/pipelines/depot.nix
+++ ops/pipelines/depot.nix
@@ -34,10 +34,10 @@ let
           nix-build -A ci.gcroot --out-link /nix/var/nix/gcroots/buildkite/canon
         '';
 
-        # Ensure that anchoring happens on build02, so that a possibly deployed
+        # Ensure that anchoring happens on build03, so that a possibly deployed
         # binary cache there has the store paths. Unanchored machines may
         # garbage collect live paths.
-        agents.hostname = "build02";
+        agents.hostname = "build03";
       }
     ];
   };
