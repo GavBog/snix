2cbfabf90dfb060362bfa642bcadca46db649b74
Vladimir Fetisov <vladimir.fetisov@pm.me>
2026-03-22 14:00:23 +0100

    fix(glue/tests): use github url in last fetch in fetchtarball case
    If you run from the project root:
      nix-instantiate --eval --strict ./snix/glue/src/tests/snix_tests/eval-okay-fetchtarball.nix
    
    the execution fails with
    
    ```
    unpacking
    'https://github.com/NixOS/nixpkgs/archive/91050ea1e57e50388fa87a3302ba12d188ef723a.tar.gz'
    into the Git cache...
    unpacking 'https://test.example/owo' into the Git cache...
    warning: error: unable to download 'https://test.example/owo': Could not
    resolve hostname (6) Could not resolve host: test.example; retrying in
    306 ms
    warning: error: unable to download 'https://test.example/owo': Could not
    resolve hostname (6) Could not resolve host: test.example; retrying in
    517 ms
    warning: error: unable to download 'https://test.example/owo': Could not
    resolve hostname (6) Could not resolve host: test.example; retrying in
    1191 ms
    warning: error: unable to download 'https://test.example/owo': Could not
    resolve hostname (6) Could not resolve host: test.example; retrying in
    2475 ms
    error:
           … while evaluating list element at index 5
           … while calling the 'fetchTarball' builtin
             at /test.nix:37:4:
               36|   # … so changing name causes the hash to change.
               37|   (builtins.fetchTarball {
                 |    ^
               38|     name = "some-name";
           … while fetching the input 'https://test.example/owo'
           error: Failed to open archive (Source threw exception: error:
                  … during download of 'https://test.example/owo'
                  error: unable to download 'https://test.example/owo':
    Could not resolve hostname (6) Could not resolve host: test.example)
    ```
    
    changing url the one from the first statement fixes that. Reusing github url
    from the first statement makes sense since all other statements
    track differences against it (e.g. change url from github to test.example, change hash, etc.)
    
    Change-Id: Id3fb14d98432f06de9bbe4570e5a1965f67c34f5
    Reviewed-on: https://cl.snix.dev/c/snix/+/31113
    Tested-by: besadii
    Reviewed-by: Florian Klink <flokli@flokli.de>

--- snix/glue/src/tests/snix_tests/eval-okay-fetchtarball.nix
+++ snix/glue/src/tests/snix_tests/eval-okay-fetchtarball.nix
@@ -20,23 +20,17 @@
     sha256 = "sha256-tRHbnoNI8SIM5O5xuxOmtSLnswEByzmnQcGGyNRjxsE=";
   })
 
-  # also with an entirely different url, it doesn't change
-  (builtins.fetchTarball {
-    url = "https://test.example/owo";
-    sha256 = "sha256-tRHbnoNI8SIM5O5xuxOmtSLnswEByzmnQcGGyNRjxsE=";
-  })
-
   # … because `name` defaults to source, and that (and the sha256 affect the store path)
   (builtins.fetchTarball {
     name = "source";
-    url = "https://test.example/owo";
+    url = "https://github.com/NixOS/nixpkgs/archive/91050ea1e57e50388fa87a3302ba12d188ef723a.tar";
     sha256 = "sha256-tRHbnoNI8SIM5O5xuxOmtSLnswEByzmnQcGGyNRjxsE=";
   })
 
   # … so changing name causes the hash to change.
   (builtins.fetchTarball {
     name = "some-name";
-    url = "https://test.example/owo";
+    url = "https://github.com/NixOS/nixpkgs/archive/91050ea1e57e50388fa87a3302ba12d188ef723a.tar.gz";
     sha256 = "sha256-tRHbnoNI8SIM5O5xuxOmtSLnswEByzmnQcGGyNRjxsE=";
   })
 ]
