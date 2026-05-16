b865c8f87ae0f52e8cb02849bfcd5edf10a522d9
Florian Klink <flokli@flokli.de>
2026-05-16 12:09:59 +0300

    feat(ops/machines/build03): copy new store paths to cache.snix.dev
    We still should probably also signing here.
    
    Change-Id: Iede086989b30a22248d1368847d1283780ef7d04
    Reviewed-on: https://cl.snix.dev/c/snix/+/31211
    Tested-by: besadii
    Reviewed-by: Brian Olsen <brian@maven-group.org>

--- ops/machines/build03/watch-store.nix
+++ ops/machines/build03/watch-store.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, depot, ... }:
 
 {
   systemd.services."watch-store@" = {
@@ -31,5 +31,34 @@
     wantedBy = [ "sockets.target" ];
     socketConfig.ListenStream = "/run/watch-store.sock";
     socketConfig.Accept = "yes";
+  };
+
+  systemd.services.snix-copy = {
+    environment.OTEL_SERVICE_NAME = "snix.store.copy";
+    environment.TRACER = "otlp";
+    environment.BLOB_SERVICE_ADDR = "grpc+https://cache.snix.dev";
+    environment.DIRECTORY_SERVICE_ADDR = "grpc+https://cache.snix.dev";
+    environment.PATH_INFO_SERVICE_ADDR = "grpc+https://cache.snix.dev";
+    serviceConfig = {
+      ExecStart = "${depot.snix.cli.store.with-features-otlp}/bin/snix-store copy --jsonl /run/watch-store.sock";
+      Type = "simple";
+      User = "snix-copy";
+      DynamicUser = true;
+      ProtectHome = true;
+      ProtectSystem = true;
+      MemoryDenyWriteExecute = true;
+      ProtectControlGroups = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      SystemCallArchitectures = "native";
+      SystemCallFilter = [
+        "@system-service"
+        "~@privileged"
+      ];
+
+    };
+    wantedBy = [ "multi-user.target" ];
   };
 }
