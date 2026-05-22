3f481dac42fba6e38b770401235c497bcb3236cc
Florian Klink <flokli@flokli.de>
2026-05-22 15:36:54 +0200

    refactor(castore): simplify CombinedBlobService
    We don't need the AsRef thing, and can #[derive(Clone)].
    
    Change-Id: If24f3890ee1d3273d5a83bf06745e0e710371677
    Reviewed-on: https://cl.snix.dev/c/snix/+/31234
    Autosubmit: Florian Klink <flokli@flokli.de>
    Reviewed-by: Brian Olsen <brian@maven-group.org>
    Tested-by: besadii

--- snix/castore/src/blobservice/combinator.rs
+++ snix/castore/src/blobservice/combinator.rs
@@ -15,22 +15,27 @@ use super::{BlobReader, BlobService, BlobWriter, ChunkedReader};
 /// blobservice for chunks, and try to read each of these chunks from the near
 /// blobservice again, before falling back to the far one.
 /// The far BlobService is never written to.
-pub struct CombinedBlobService<BL, BR> {
+#[derive(Clone)]
+pub struct CombinedBlobService<BL, BR>
+where
+    BL: Clone,
+    BR: Clone,
+{
     instance_name: String,
     near: BL,
     far: BR,
 }
 
-impl<BL, BR> Clone for CombinedBlobService<BL, BR>
+impl<BL, BR> CombinedBlobService<BL, BR>
 where
     BL: Clone,
     BR: Clone,
 {
-    fn clone(&self) -> Self {
+    pub fn new(instance_name: String, near: BL, far: BR) -> Self {
         Self {
-            instance_name: self.instance_name.clone(),
-            near: self.near.clone(),
-            far: self.far.clone(),
+            instance_name,
+            near,
+            far,
         }
     }
 }
@@ -38,19 +43,19 @@ where
 #[async_trait]
 impl<BL, BR> BlobService for CombinedBlobService<BL, BR>
 where
-    BL: AsRef<dyn BlobService> + Clone + Send + Sync + 'static,
-    BR: AsRef<dyn BlobService> + Clone + Send + Sync + 'static,
+    BL: BlobService + Clone + 'static,
+    BR: BlobService + Clone + 'static,
 {
     #[instrument(skip(self, digest), fields(blob.digest=%digest, instance_name=%self.instance_name))]
     async fn has(&self, digest: &B3Digest) -> std::io::Result<bool> {
-        Ok(self.near.as_ref().has(digest).await? || self.far.as_ref().has(digest).await?)
+        Ok(self.near.has(digest).await? || self.far.has(digest).await?)
     }
 
     #[instrument(skip(self, digest), fields(blob.digest=%digest, instance_name=%self.instance_name), err)]
     async fn open_read(&self, digest: &B3Digest) -> std::io::Result<Option<Box<dyn BlobReader>>> {
-        if self.near.as_ref().has(digest).await? {
+        if self.near.has(digest).await? {
             // near store has the blob, so we can assume it also has all chunks.
-            self.near.as_ref().open_read(digest).await
+            self.near.open_read(digest).await
         } else {
             // near store doesn't have the blob.
             // Ask the remote one for the list of chunks,
@@ -58,7 +63,7 @@ where
             // individual chunks. There's a chance we already have some chunks
             // in near, meaning we don't need to fetch them all from the far
             // BlobService.
-            match self.far.as_ref().chunks(digest).await? {
+            match self.far.chunks(digest).await? {
                 // blob doesn't exist on the near side either, nothing we can do.
                 None => Ok(None),
                 Some(remote_chunks) => {
@@ -66,7 +71,7 @@ where
                     // blobservice doesn't support chunks, read the blob from
                     // the far blobservice directly.
                     if remote_chunks.is_empty() {
-                        return self.far.as_ref().open_read(digest).await;
+                        return self.far.open_read(digest).await;
                     }
                     // otherwise, a chunked reader, which will always try the
                     // near backend first.
@@ -89,7 +94,7 @@ where
     #[instrument(skip_all, fields(instance_name=%self.instance_name))]
     async fn open_write(&self) -> Box<dyn BlobWriter> {
         // direct writes to the near one.
-        self.near.as_ref().open_write().await
+        self.near.open_write().await
     }
 }
 
@@ -115,8 +120,10 @@ impl ServiceBuilder for CombinedBlobServiceConfig {
         instance_name: &str,
         context: &CompositionContext,
     ) -> Result<Arc<Self::Output>, Box<dyn std::error::Error + Send + Sync>> {
-        let (local, remote) =
-            futures::join!(context.resolve(&self.near), context.resolve(&self.far));
+        let (local, remote) = futures::join!(
+            context.resolve::<dyn BlobService>(&self.near),
+            context.resolve::<dyn BlobService>(&self.far)
+        );
         Ok(Arc::new(CombinedBlobService {
             instance_name: instance_name.to_string(),
             near: local?,
