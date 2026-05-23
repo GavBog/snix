c0be52d11e9a464e00a3415f56cf1156858cebbf
Brian Olsen <brian@maven-group.org>
2026-05-23 20:53:19 +0200

    feat(tools): Add rebase-exec tool
    This adds a rebase-exec tool to run command on every commit since canon and
    add the changes made by that command to the commit.
    
    Change-Id: I48c97511c336f37e7e1cd67af140bedb6326a43e
    Reviewed-on: https://cl.snix.dev/c/snix/+/31250
    Autosubmit: Brian Olsen <brian@maven-group.org>
    Reviewed-by: Florian Klink <flokli@flokli.de>
    Tested-by: besadii

---
+++ tools/rebase-exec.nix
@@ -0,0 +1,27 @@
+{ pkgs, depot, ... }:
+
+# Run a command for each commit since canon and include the files it changes in
+# the commit
+pkgs.writeShellScriptBin "rebase-exec" ''
+  export PATH="${
+    pkgs.lib.makeBinPath [
+      pkgs.findutils
+      pkgs.git
+    ]
+  }:$PATH"
+  REPO_ROOT="''${MG_ROOT:-$(git rev-parse --show-toplevel)}"
+  cd "$REPO_ROOT"
+  echo "Running:" git rebase --exec "$*" canon
+  if ! git rebase --exec "$*" canon ; then
+    REBASE_DIR="$(git rev-parse --absolute-git-dir)/rebase-merge"
+    REBASE_DIR2="$(git rev-parse --absolute-git-dir)/rebase-apply"
+    while [[ -d "$REBASE_DIR" ]] || [[ -d "$REBASE_DIR2" ]] && [[ -n "$(git status --porcelain -uno)" ]]; do
+      git add -u
+      git commit --amend --no-edit
+      git rebase --continue
+    done
+    if [[ -d "$REBASE_DIR" ]] || [[ -d "$REBASE_DIR2" ]] ; then
+      git rebase --abort
+    fi
+  fi
+''
