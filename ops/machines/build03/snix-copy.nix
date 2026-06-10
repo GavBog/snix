0ad06e48fd09b3654973fdb1e473220435df9aa2
Florian Klink <flokli@flokli.de>
2026-06-10 22:04:19 +0300

    feat(ops/machines/build03/snix-copy): sign PathInfos before uploading
    This uses the same binary cache key that harmonia used to sign PathInfos
    uploaded to cache.snix.dev.
    
    Part of https://git.snix.dev/snix/snix/issues/198.
    
    Change-Id: I48c6e54c2b39bc2dc4e5acbe4d96667eaecdc270
    Reviewed-on: https://cl.snix.dev/c/snix/+/31344
    Tested-by: besadii
    Reviewed-by: picnoir <picnoir@alternativebit.fr>
    Autosubmit: Florian Klink <flokli@flokli.de>

--- ops/machines/build03/snix-copy.nix
+++ ops/machines/build03/snix-copy.nix
@@ -1,23 +1,57 @@
 {
   depot,
   config,
+  pkgs,
   ...
 }:
 
 let
   tls_client_cert_path = depot.ops.pki.host_certificates."build03.infra.snix.dev";
   tls_client_key_path = "/run/credentials/snix-copy.service/mtls-private-key.pem";
-  storeUrl = "grpc+https://cache.snix.dev?tls-client-cert-path=${tls_client_cert_path}&tls-client-key-path=${tls_client_key_path}";
+  binary_cache_key_path = "/run/credentials/snix-copy.service/binary-cache-key";
+  store_url = "grpc+https://cache.snix.dev?tls-client-cert-path=${tls_client_cert_path}&tls-client-key-path=${tls_client_key_path}";
+
+  storeCompositionFile = (pkgs.formats.toml { }).generate "store-composition.toml" {
+    blobservices = {
+      root = {
+        type = "grpc";
+        url = store_url;
+      };
+    };
+
+    directoryservices = {
+      root = {
+        type = "grpc";
+        url = store_url;
+      };
+    };
+
+    pathinfoservices = {
+      root = {
+        type = "keyfile-signing";
+        keyfile = binary_cache_key_path;
+        # sign, then upload via gRPC.
+        inner = "&grpc";
+      };
+      grpc = {
+        type = "grpc";
+        url = store_url;
+      };
+    };
+
+  };
 in
 {
+  age.secrets.binary-cache-key.file = depot.ops.secrets."binary-cache-key.age";
+
   systemd.services.snix-copy = {
     environment.OTEL_SERVICE_NAME = "snix.store.copy";
     environment.TRACER = "otlp";
-    environment.BLOB_SERVICE_ADDR = storeUrl;
-    environment.DIRECTORY_SERVICE_ADDR = storeUrl;
-    environment.PATH_INFO_SERVICE_ADDR = storeUrl;
     serviceConfig = {
-      ExecStart = "${depot.snix.cli.store.with-features-otlp}/bin/snix-store copy --jsonl /run/watch-store.sock";
+      ExecStart =
+        "${depot.snix.cli.store.with-features-xp-store-composition-cli-otlp}/bin/snix-store copy"
+        + " --experimental-store-composition ${storeCompositionFile}"
+        + " --jsonl /run/watch-store.sock";
       Type = "simple";
       User = "snix-copy";
       DynamicUser = true;
@@ -38,6 +72,7 @@ in
       ];
       LoadCredential = [
         "mtls-private-key.pem:${config.age.secrets.mtls-private-key.path}"
+        "binary-cache-key:${config.age.secrets.binary-cache-key.path}"
       ];
     };
     wantedBy = [ "multi-user.target" ];
